3e5e770b72ab9b0fd9b1e92c7613a3a762b39665
HrishikeshPawar <rushipawar197@gmail.com>
2023-05-05 13:56:37 +0530

    new 2 codes uploaded

---
+++ Q7pattern1.java
@@ -0,0 +1,19 @@
+package Assignment2;
+/***********
+**********
+**********
+***********/
+public class Q7pattern1 {
+
+	public static void main(String[] args) {
+		for (int i=1;i<=5;i++)//row
+		{
+			for(int j=1;j<=10;j++)//column
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+}
